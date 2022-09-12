8e7a023e0b9bcd6c3a7b28d7a18d79d691aed01d
root <root@localhost>
2022-09-12 01:24:21 +0000

    book project update

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: '30.5'
+    fill_in 'Published_Date', with: '1/1/2021'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('JK Rowling')
   end
 end
