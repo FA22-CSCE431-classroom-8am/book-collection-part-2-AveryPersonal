81b27743a93f37a14e872dcb188fa857ad22cc32
root <root@localhost>
2022-09-14 03:27:17 +0000

    final update

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'JK Rowling'
     fill_in 'Price', with: '30.5'
-    #fill_in 'Published-Date', with: '01/01/2021'
+    fill_in 'Published date', with: '01/01/2021'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
     expect(page).to have_content('JK Rowling')
     expect(page).to have_content('30.5')
-    #expect(page).to have_content('01/01/2021')
+    expect(page).to have_content('2021-01-01')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -7,27 +7,55 @@ RSpec.describe Book, type: :model do
 
   end
 
+  #1
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
-
+  #2
   it 'is not valid without a title' do
     subject.title = nil
     expect(subject).not_to be_valid
   end
-
+  #3
   it 'is not valid without an author' do
     subject.author = nil
     expect(subject).not_to be_valid
   end
-
+  #4
   it 'is not valid without a price' do
     subject.price = nil
     expect(subject).not_to be_valid
   end
 
+  #5
+  it 'is not valid without a published date' do
+    subject.published_date = nil
+    expect(subject).not_to be_valid
+  end
+
+  #subject do
+    #described_class.new(title: 'harry potter', author: 'JK Rowling',price:'0',published_date:'01/01/2021')
+
+  #end
+  #6
+  it 'is not valid price must be greater than 0' do
+    subject.price = -1
+    expect(subject).not_to be_valid
+  end
+
+  #7
+  it 'is not valid price must be a number' do
+    subject.price = 'a.1'
+    expect(subject).not_to be_valid
+  end
+
   #it 'is not valid without a published date' do
-    #subject.published_date = nil
-    #expect(subject).not_to be_valid
+  #  subject.published_date = nil
+  #  expect(subject).not_to be_valid
+  #end
+
+  #it 'is not valid without a published date' do
+  #  subject.published_date = nil
+  #  expect(subject).not_to be_valid
   #end
 end
