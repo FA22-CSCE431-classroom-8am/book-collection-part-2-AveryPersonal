02a9fb7c42c6d47ffdba79295b85430f7cd359c8
root <root@localhost>
2022-09-19 18:09:18 +0000

    book collection part 2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
+	has_many :users, through: :user_books
+	has_many :user_books
+	
 	validates :title, presence: true
-	validates:author,:published_date, presence: true
+	validates :author,:published_date, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0 }
 	#validates_date :published_date, date: {before: Date.today}
 	#validates :published_date, timeliness: { on_or_before: lambda { Date.current }, type: :date }
